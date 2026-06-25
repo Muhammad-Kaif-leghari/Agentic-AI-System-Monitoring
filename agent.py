d7ea8f4350f67ec095d29114a21e219bb84b9b73
Muhammad Kaif <kaifleghari92@gmail.com>
2026-06-25 21:23:31 +0500

    implement interactive shell loop and fix get_ram_utilization tool metrics

--- agent.py
+++ agent.py
@@ -19,23 +19,45 @@ def get_cpu_utilization() -> str:
     cpu_percentage = psutil.cpu_percent(interval=1)
     return f"Current Cpu Usage is: {cpu_percentage}%"
 
+def get_ram_utilization() -> str:
+    """
+    Checks and returns the current RAM (Memory) usage percentage of the local computer.
+    Use this tool whenever the user asks about memory, RAM, background apps consuming space,
+    or if they ask if they have enough free memory left to run programs.
+    """
+    ram_info = psutil.virtual_memory()
+    return f"Current Ram Usage is: {ram_info.percent}% (Used {ram_info.used / (1024**3):.2f} GB / Total: {ram_info.total / (1024**3): .2f} GB)" 
+
 # We pass our Python function inside a list to the config object
-agent_tools = [get_cpu_utilization]
-
-print("Agent is online and monitoring system health...")
+agent_tools = [get_cpu_utilization, get_ram_utilization]
+print("\n")
+print("System Monitoring Agent is Online! Type 'exit', 'quit', 'q' or 'close' to stop.")
 print("-" * 50)
+ 
+while True:
+    user_prompt = input("\nYou:")
 
-user_prompt = "Hey! My computer feels like it is lagging a bit. Can you check my system health and tell me if my CPU is working too hard?"
-print(f"User: {user_prompt}\n")
+    if user_prompt.lower() in ["exit", "quit", "q", "close"]:
+        print("Shutting down agent. Goodbye!")
+        break
+        
+    if not user_prompt.strip():
+        continue
 
-# 3. Send a simple message to the flash model
-response = client.models.generate_content(
-    model='gemini-2.5-flash',
-    contents=user_prompt,
-    config=types.GenerateContentConfig(
-    tools=agent_tools,
-)
-)
+    
+    print("Agent is processing...")
 
-print("\nGemini Response:")
-print(response.text)
+    try:
+        # 3. Send a simple message to the flash model
+        response = client.models.generate_content(
+            model='gemini-2.5-flash',
+            contents=user_prompt,
+            config=types.GenerateContentConfig(
+                tools=agent_tools,
+            )
+        )
+
+        print(f"\nGemini Response: {response.text}")
+
+    except Exception as e:
+        print(f"An Error occurred: {e}")
